c8d3c562f93c3fea70c998d843196b8758473d1e
mayuresh-tech9 <mayuresh.gramopadhye@tech9.com>
2023-10-13 15:52:05 +0530

    UI improvements

--- resources/views/livewire/log-time-component.blade.php
+++ resources/views/livewire/log-time-component.blade.php
@@ -1,118 +1,101 @@
-{{--<div class="flex justify-center items-center">--}}
-{{--    <h1>Log Time: {{ $time }}</h1>--}}
-{{--</div>--}}
-<div class="flex justify-center items-center border-2 m-5 bg-gray-50">
-    <form wire:submit.prevent="submitForm">
-        <div class="flex justify-center items-center border-2 m-1.5">
-            <h1>Date: {{ $time }}</h1>
-        </div>
-        <div>
-            <div class="flex justify-center items-center border-2 m-1.5 p-1.5">
-                <select wire:model="selectedItem" id="dropdown">
+<div class="flex justify-center items-center bg-white border-2 mx-2 p-0 rounded-lg shadow-lg relative">
+    <h1 class="absolute top-0 left-0 mt-2 ml-2 text-blue-400">Date: {{ $time }}</h1>
+    <form wire:submit.prevent="submitForm" class="w-full">
+    <div class="mt-4">
+            <div class="flex justify-center items-center border-b-2 p-3">
+                <select wire:model="selectedItem" id="dropdown" class="w-1/2">
                     <option value="">Select project</option>
                     @foreach ($projects as $key => $value)
                         <option value="{{ $key }}">{{ $key }}</option>
                     @endforeach
                 </select>
-                <button wire:click="getCommits" class="@if(!$selectedItem) bg-gray-400 @else bg-blue-400 @endif text-white rounded p-1.5 ml-2" @if(!$selectedItem) disabled @endif>Get Commits</button>
+                <button wire:click="getCommits" class="bg-blue-400 text-white rounded p-2 ml-4" @if(!$selectedItem) disabled @endif>
+                    Get Commits
+                </button>
             </div>
-                <div class="flex justify-center items-center border-2 m-1.5 p-1.5">
-                    <select wire:model="selectedDesc" id="dropdown" @if(!$selectedItem) disabled @endif>
-                        <option value="">Select an commit</option>
-                        @foreach ($output as $key => $value)
-                            <option value="{{ substr($value, strpos($value, ' ') + 1) }}">{{ substr($value, strpos($value, ' ') + 1) }}</option>
-                        @endforeach
-                    </select>
-                    <button wire:click="appendString" class="@if(!$selectedDesc) bg-gray-400 @else bg-blue-400 @endif text-white rounded p-1.5 ml-2" @if(!$selectedDesc) disabled @endif>Add</button>
-                </div>
-        </div>
-        <div>
-
-
-            <div class="flex justify-center items-center border-2">
-                <select wire:model="selectedProjectId" id="projectList">
-                    <option value="">Select Project...</option>
-                    @foreach ($projectAssignments as $assignment)
-                        <option value="{{ $assignment['project']['id'] }}">{{ $assignment['client']['name'] }} - {{ $assignment['project']['name'] }}</option>
+            <div class="flex justify-center items-center border-b-2 p-3" @if(!$selectedItem) style="opacity: 0.6;" @endif>
+                <select wire:model="selectedDesc" id="dropdown" class="w-1/2" @if(!$selectedItem) disabled @endif>
+                    <option value="">Select a commit</option>
+                    @foreach ($output as $value)
+                        <option value="{{ substr($value, strpos($value, ' ') + 1) }}">{{ substr($value, strpos($value, ' ') + 1) }}</option>
                     @endforeach
                 </select>
+                <button wire:click="appendString" class="bg-blue-400 text-white rounded p-2 ml-4 @if(!$selectedDesc) disabled @endif" @if(!$selectedDesc) disabled @endif>
+                    Add
+                </button>
             </div>
-                @if ($selectedProjectId)
-                    <div class="flex justify-center items-center border-2">
-                        <select wire:model="selectedTaskId" id="taskList">
-                            <option value="">Select a Task</option>
-                            @foreach ($projectAssignments as $assignment)
-                                @if ($assignment['project']['id'] == $selectedProjectId)
-                                    @foreach ($assignment['task_assignments'] as $taskAssignment)
-                                        <option value="{{ $taskAssignment['task']['id'] }}">{{ $taskAssignment['task']['name'] }}</option>
-                                    @endforeach
-                                @endif
+        </div>
+        <div class="mt-4 flex justify-center items-center border-b-2 p-3">
+            <select wire:model="selectedProjectId" id="projectList" class="w-1/2">
+                <option value="">Select Project...</option>
+                @foreach ($projectAssignments as $assignment)
+                    <option value="{{ $assignment['project']['id'] }}">{{ $assignment['client']['name'] }} - {{ $assignment['project']['name'] }}</option>
+                @endforeach
+            </select>
+            @if ($selectedProjectId)
+                <select wire:model="selectedTaskId" id="taskList" class="w-1/2 ml-4">
+                    <option value="">Select a Task</option>
+                    @foreach ($projectAssignments as $assignment)
+                        @if ($assignment['project']['id'] == $selectedProjectId)
+                            @foreach ($assignment['task_assignments'] as $taskAssignment)
+                                <option value="{{ $taskAssignment['task']['id'] }}">{{ $taskAssignment['task']['name'] }}</option>
                             @endforeach
-                        </select>
+                        @endif
+                    @endforeach
+                </select>
+            @endif
+        </div>
+
+        <label class="flex justify-center items-center border-b-2 p-3 hidden">
+            <input type="checkbox" wire:model="setAsDefault" wire:click="toggleCheckbox" class="text-blue-400">
+            Set project as default
+        </label>
+        <div class="mt-4 flex items-start space-x-4">
+            <textarea wire:model="description" id="time_entry" rows="2" class="border-2 rounded w-1/2 p-2" placeholder="Description"></textarea>
+            <input type="text" wire:model="hours" id="time_entry_hours" class="border-2 rounded w-1/2 p-2" placeholder="8:00" />
+        </div>
+        <div class="mt-4 flex justify-center items-center">
+            <button type="submit" class="bg-blue-400 text-white rounded p-2 hidden">Submit</button>
+            <button wire:click="toggleSubmit" class="bg-blue-400 text-white rounded p-2 ml-4 @if($idToEdit) hidden @endif" @if($idToEdit) disabled @endif>
+                Submit
+            </button>
+            @if($idToEdit)
+                <button wire:click="setUpdated" class="bg-blue-400 text-white rounded p-2 ml-4">Update</button>
+                <button wire:click="cancelEdit" class="bg-gray-400 text-white rounded p-2 ml-4">Cancel</button>
+            @endif
+        </div>
+        @if(!$idToEdit)
+            <div class="mt-4">
+                @if(count($existingEntries) > 0)
+                    <div class="p-4 rounded-lg shadow-lg">
+                        <h1 class="text-xl font-bold mb-4 text-blue-400">Time entries</h1>
+                        <table class="w-full">
+                            <thead>
+                            <tr class="bg-blue-400 text-white">
+                                <th class="p-3">Project</th>
+                                <th class="p-3">Notes</th>
+                                <th class="p-3">Hours</th>
+                                <th class="p-3">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($existingEntries as $item)
+                                <tr>
+                                    <td class="p-3">{{ $item['project']['name'] . ' - ' . $item['task']['name'] }}</td>
+                                    <td class="p-3">{{ $item['notes'] }}</td>
+                                    <td class="p-3">{{ $item['hours'] }}</td>
+                                    <td class="p-3">
+                                        <button wire:click="editLog({{ $item['id'] }})" class="bg-blue-400 text-white rounded p-2">
+                                            Edit
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 @endif
-                <label class="flex justify-center items-center border-2">
-                    <input type="checkbox" wire:model="setAsDefault" wire:click="toggleCheckbox">
-                    Set project as default
-                </label>
-        </div>
-            <div class="flex justify-center items-center">
-                <textarea wire:model="description" id="time_entry" rows="5" cols="40" class="border-2 rounded" placeholder="Description"></textarea>
             </div>
-            <div class="flex justify-center items-center">
-                <input type="text" wire:model="hours" id="time_entry_hours" class="border-2 rounded p-2 w-11/12" placeholder="8:00"  />
-            </div>
-             <div class="flex justify-center items-center">
-                    <button type="submit" class="bg-blue-400 text-white rounded p-1.5 mt-2 hidden" >Submit</button>
-                    <button wire:click="toggleSubmit" class="bg-blue-400 text-white rounded p-1.5 mt-2 @if($idToEdit) hidden @endif" @if($idToEdit) disabled  @endif>Submit</button>
-            @if($idToEdit)
-                <button wire:click="setUpdated" class="bg-blue-400 text-white rounded p-1.5 mt-2">Update</button>
-                <button wire:click="cancelEdit" class="bg-gray-400 text-white rounded p-1.5 mt-2">Cancel</button>
-           @endif
-        </div>
-            @if(!$idToEdit)
-                <div class="flex justify-center items-center border-2 m-5">
-                    @if(count($existingEntries) > 0)
-                        <div class="flex justify-center items-center">
-                            <div class="p-4 rounded-lg shadow-lg">
-                                <h1 class="text-xl font-bold mb-4">Time entries</h1>
-                                <table>
-                                    <thead class="bg-blue-400 text-white">
-                                    <tr>
-                                        <td class="px-4 py-2">Project</td>
-                                        <td class="px-4 py-2">Notes</td>
-                                        <td class="px-4 py-2">Hours</td>
-                                        <td class="px-4 py-2">Action</td>
-                                    </tr>
-
-                                    </thead>
-                                    <tbody>
-
-                                    @foreach ($existingEntries as $key => $item)
-                                        <tr>
-                                            <!-- Your table rows here -->
-                                            <td class="px-4 py-2">{{$item['project']['name'] . '-' . $item['task']['name']}}</td>
-                                            <td class="px-4 py-2">
-                                                {{$item['notes']}}
-                                            </td>
-                                            <td class="px-4 py-2">
-                                                {{$item['hours']}}
-                                            </td>
-                                            <td class="px-4 py-2">
-                                                <button wire:click="editLog({{ $item['id'] }})" class="bg-blue-400 text-white rounded p-1.5">Edit</button>
-                                            </td>
-
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    @endif
-                </div>
-
-            @endif
-
+        @endif
     </form>
 </div>
-
--- resources/views/livewire/logs.blade.php
+++ resources/views/livewire/logs.blade.php
@@ -1,169 +1,140 @@
-<div>
-    <div class="flex justify-center items-center">
-    <div class="grid-cols-1 w-6/12 ml-10">
-        <div class="grid grid-cols-3">
-        <a href="{{route('logs')}}" >
-            &#8635; Refresh
-        </a>
+<div class="p-4">
+  <div class="flex justify-start items-center space-x-4 ml-10">
+    <a href="{{ route('logs') }}" class="btn-refresh">&#8635; Refresh</a>
+    <button wire:click="toggleDetails" class="btn-details">
+      @if($showDetails)
+        Hide Details
+      @else
+        Show Details
+      @endif
+    </button>
+    <a href="{{ route('auto-import') }}" class="btn-auto-import @if(!$showDetails) disabled @endif">Auto Import</a>
+  </div>
 
-        <button wire:click="toggleDetails">
-            @if($showDetails)
-                <h>Hide Details</h>
-            @else
-                Show Details
-            @endif
-        </button>
-            @if($showDetails)
-                <a href="{{route('auto-import')}}">
-                    Auto import
-                </a>
-            @endif
+  @if(count($data['missingEntries']) === 0 && count($data['lesserHours']) === 0 && count($data['missingNotes']) === 0)
+    <div class="flex justify-center items-center mt-8">
+      <div class="p-4 rounded-lg shadow-lg">
+        <h1 class="text-xl font-bold mb-4">You are awesome!!!</h1>
+      </div>
+    </div>
+  @elseif($showDetails)
+    @if(count($data['missingEntries']) > 0)
+      <div class="flex justify-center items-center mt-8">
+        <div class="p-4 rounded-lg shadow-lg">
+          <h1 class="text-xl font-bold mb-4">Missing Time Entries</h1>
+          <table class="w-full">
+            <thead class="bg-blue-400 text-white">
+            <tr>
+              <th class="px-4 py-2">Date</th>
+              <th class="px-4 py-2">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($data['missingEntries'] as $item)
+              <tr>
+                <td class="px-4 py-2">{{ $item }}</td>
+                <td class="px-4 py-2">
+                  <a href="/log-time/{{ $item }}" class="btn-add">Add</a>
+                </td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
         </div>
-
-    </div></div>
-    @if(count($data['missingEntries']) === 0 && count($data['lesserHours']) === 0 && count($data['missingNotes']) === 0)
-        <div class="flex justify-center items-center">
-            <div class="p-4 rounded-lg shadow-lg">
-                <h1 class="text-xl font-bold mb-4">You are awesome!!!</h1>
-                Jason Loves you :)
-            </div>
-        </div>
-    @elseif($showDetails)
-        @if(count($data['missingEntries']) > 0)
-            <div class="flex justify-center items-center">
-                <div class="p-4 rounded-lg shadow-lg">
-                    <h1 class="text-xl font-bold mb-4">Missing time entries</h1>
-                    <table>
-                        <thead class="bg-blue-400 text-white">
-                        <tr>
-                            <td class="px-4 py-2">Date</td>
-                            <td class="px-4 py-2">Action</td>
-                        </tr>
-
-                        </thead>
-                        <tbody>
-
-                        @foreach ($data['missingEntries'] as $key => $item)
-                            <tr>
-                                <!-- Your table rows here -->
-                                <td class="px-4 py-2">
-                                    {{$item}}
-                                </td>
-                                <td class="px-4 py-2">
-                                    <a href="/log-time/{{$item}}"> Add </a>
-                                </td>
-
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        @endif
-        @if(count($data['lesserHours']) > 0)
-                <div class="flex justify-center items-center">
-                    <div class="p-4 rounded-lg shadow-lg">
-                        <h1 class="text-xl font-bold mb-4">Dates missing hours</h1>
-                    <table>
-                        <thead class="bg-blue-400 text-white">
-                        <tr class="bg-blue-400 text-white">
-                        <tr>
-                            <td class="px-4 py-2">Date</td>
-                            <td class="px-4 py-2">Action</td>
-                        </tr>
-
-                        </thead>
-                        <tbody>
-
-                        @foreach ($data['lesserHours'] as $key => $item)
-                            <tr>
-                                <!-- Your table rows here -->
-                                <td class="px-4 py-2">
-                                    {{$item}}
-                                </td>
-                                <td class="px-4 py-2">
-                                    <a href="/log-time/{{$item}}"> Add/Update </a>
-                                </td>
-
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        @endif
-        @if(count($data['missingNotes']) > 0)
-                <div class="flex justify-center items-center">
-                    <div class="p-4 rounded-lg shadow-lg">
-                        <h1 class="text-xl font-bold mb-4">Dates missing notes</h1>
-                        <table>
-                            <thead class="bg-blue-400 text-white">
-                            <tr class="bg-blue-400 text-white">
-                                <td class="px-4 py-2">Date</td>
-                                <td class="px-4 py-2">Action</td>
-                            </tr>
-
-                            </thead>
-                            <tbody>
-
-                            @foreach ($data['missingNotes'] as $key => $item)
-                                <tr>
-                                    <!-- Your table rows here -->
-                                    <td class="px-4 py-2">
-                                        {{$item}}
-                                    </td>
-                                    <td class="px-4 py-2">
-                                        <a href="/log-time/{{$item}}"> Add notes </a>
-                                    </td>
-
-                                </tr>
-                            @endforeach
-                            </tbody>
-                    </table>
-                </div>
-            </div>
-        @endif
-    @else
-        <div class="flex justify-center items-center">
-            <div class="grid-cols-1 w-6/12 ml-20">
-        <div class="grid grid-cols-2 gap-2">
-            <div class="grid-cols-1">
-                <div class="p-4 text-center bg-blue-100">
-                    Total Hours
-                </div>
-                <div class="p-4 text-center">
-                    {{ $data['totalHours'] }}
-                </div>
-            </div>
-            <div class="grid-cols-1">
-                <div class="p-4 text-center bg-blue-100">
-                    Billable Hours
-                </div>
-                <div class="p-4 text-center">
-                    {{ $data['billableHours'] }}
-                </div>
-            </div>
-        </div>
-        <div class="grid grid-cols-2 gap-2">
-            <div class="grid-cols-1">
-                <div class="p-4 text-center bg-blue-100">
-                    Non-billable Hours
-                </div>
-                <div class="p-4 text-center">
-                    {{ $data['nonBillableHours'] }}
-                </div>
-            </div>
-            <div class="grid-cols-1">
-                <div class="p-4 text-center bg-blue-100">
-                    Missing Days
-                </div>
-                <div class="p-4 text-center">
-                    {{ count($data['missingEntries']) }}
-                </div>
-            </div>
-        </div>
-        </div>
-        </div>
+      </div>
     @endif
 
+    @if(count($data['lesserHours']) > 0)
+      <div class="flex justify-center items-center mt-8">
+        <div class="p-4 rounded-lg shadow-lg">
+          <h1 class="text-xl font-bold mb-4">Dates Missing Hours</h1>
+          <table class="w-full">
+            <thead class="bg-blue-400 text-white">
+            <tr>
+              <th class="px-4 py-2">Date</th>
+              <th class="px-4 py-2">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($data['lesserHours'] as $item)
+              <tr>
+                <td class="px-4 py-2">{{ $item }}</td>
+                <td class="px-4 py-2">
+                  <a href="/log-time/{{ $item }}" class="btn-add">Add/Update</a>
+                </td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    @endif
+
+    @if(count($data['missingNotes']) > 0)
+      <div class="flex justify-center items-center mt-8">
+        <div class="p-4 rounded-lg shadow-lg">
+          <h1 class="text-xl font-bold mb-4">Dates Missing Notes</h1>
+          <table class="w-full">
+            <thead class="bg-blue-400 text-white">
+            <tr>
+              <th class="px-4 py-2">Date</th>
+              <th class="px-4 py-2">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($data['missingNotes'] as $item)
+              <tr>
+                <td class="px-4 py-2">{{ $item }}</td>
+                <td class="px-4 py-2">
+                  <a href="/log-time/{{ $item }}" class="btn-add">Add Notes</a>
+                </td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    @endif
+  @else
+    <div class="flex justify-start items-center">
+      <div class="grid-cols-1 ml-10">
+        <div class="grid grid-cols-2 gap-2">
+          <div class="grid-cols-1">
+            <div class="p-4 text-center bg-blue-100">
+              Total Hours
+            </div>
+            <div class="p-4 text-center">
+              {{ $data['totalHours'] }}
+            </div>
+          </div>
+          <div class="grid-cols-1">
+            <div class="p-4 text-center bg-blue-100">
+              Billable
+            </div>
+            <div class="p-4 text-center">
+              {{ $data['billableHours'] }}
+            </div>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 gap-2">
+          <div class="grid-cols-1">
+            <div class="p-4 text-center bg-blue-100">
+              Non-billable
+            </div>
+            <div class="p-4 text-center">
+              {{ $data['nonBillableHours'] }}
+            </div>
+          </div>
+          <div class="grid-cols-1">
+            <div class="p-4 text-center bg-blue-100">
+              Missing Days
+            </div>
+            <div class="p-4 text-center">
+              {{ count($data['missingEntries']) }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endif
 </div>
