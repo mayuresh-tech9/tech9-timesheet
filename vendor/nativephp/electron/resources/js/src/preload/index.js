53f1b19ac8d973efa783bec26f7b493d9190b8c6
mayuresh-tech9 <mayuresh.gramopadhye@tech9.com>
2023-10-26 12:59:31 +0530

    Removed vendor from tracking

--- vendor/nativephp/electron/resources/js/src/preload/index.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
-import * as remote from '@electron/remote'
-
-window.electron = electronAPI
-window.remote = remote;
